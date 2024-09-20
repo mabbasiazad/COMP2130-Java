921210c33354ec95a44409e867e0e00754a2530a
mabbasiazad <m32abbas@uwaterloo.ca>
2024-09-20 10:54:53 -0400

    third exe

--- week3/third_exercise/Main.java
+++ week3/third_exercise/Main.java
@@ -3,7 +3,8 @@ package third_exercise;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Person_Modified p1= new Person_Modified("Mehdi", 30);
+        System.out.println(p1);
     }
     
 }
