f71199508c1b869c972421b1a9a7f4464b903563
mabbasiazad <m32abbas@uwaterloo.ca>
2024-09-20 09:11:42 -0400

    second part

--- week3/third_exercise/Main.java
+++ week3/third_exercise/Main.java
@@ -3,7 +3,13 @@ package third_exercise;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Person_Modified new_person = new Person_Modified("Mehdi", 20);
+        System.out.println(new_person.getAge());  
+        new_person.display();
+
+        System.out.println();
+        System.out.println(new_person);
+
     }
     
 }
