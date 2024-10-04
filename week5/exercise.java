b9475093618a084bbdf5079e4e2f7a21dbe698ca
mabbasiazad <m32abbas@uwaterloo.ca>
2024-10-04 09:38:43 -0400

    next step: add list

--- week5/exercise.java
+++ week5/exercise.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class exercise {
     public static void main(String[] args) {
         double[][] info = {{80, 1.6}, {92, 1.7}, {88, 1.75}, {50, 1.6}};
+
+        ArrayList<Double> BMIs = new ArrayList<Double>();
     }
 }
